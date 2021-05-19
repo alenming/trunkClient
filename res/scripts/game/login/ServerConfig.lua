5e4acdbbff84a29face3fb6824be99e3bb7c8340
alenming <635459675@qq.com>
2021-05-19 09:43:16 +0800

    add

--- res/scripts/game/login/ServerConfig.lua
+++ res/scripts/game/login/ServerConfig.lua
@@ -22,18 +22,10 @@
     ----[[
     ServerConfig = 
     {
-        {Id = "1区", Name = "自己服务器", Status = "新服", Ip = "127.0.0.1", Port = 5555, WorldId = 3, ServerId = 4},
-        {Id = "2区", Name = "内网linux", Status = "火爆", Ip = "192.168.9.8", Port = 5555, WorldId = 2, ServerId = 1},
-        {Id = "3区", Name = "外网linux", Status = "新服", Ip = "120.76.166.234", Port = 9999, WorldId = 2, ServerId = 10009},
-        {Id = "4区", Name = "外网windows", Status = "火爆", Ip = "112.74.208.214", Port = 5555, WorldId = 3, ServerId = 3},
-        {Id = "5区", Name = "宝爷的服务器", Status = "新服", Ip = "192.168.0.29", Port = 5555, WorldId = 3, ServerId = 5},
-        {Id = "6区", Name = "穗勇的服务器", Status = "火爆", Ip = "192.168.3.86", Port = 5555, WorldId = 3, ServerId = 6},
-        {Id = "7区", Name = "黄成的服务器", Status = "火爆", Ip = "192.168.0.40", Port = 5555, WorldId = 3, ServerId = 7},
-        {Id = "8区", Name = "志强的服务器", Status = "火爆", Ip = "192.168.2.91", Port = 5555, WorldId = 3, ServerId = 8},
-        {Id = "9区", Name = "大帅哥的服务器", Status = "火爆", Ip = "192.168.0.185", Port = 5555, WorldId = 3, ServerId = 9},
-        {Id = "10区", Name = "杨的服务器", Status = "火爆", Ip = "192.168.0.3", Port = 5555, WorldId = 3, ServerId = 10},
-        {Id = "11区", Name = "埃米尔大陆", Status = "火爆", Ip = "139.199.207.55", Port = 1111, WorldId = 2, ServerId = 1},
-        {Id = "12区", Name = "绍坚服务器", Status = "火爆", Ip = "192.168.3.57", Port = 5555, WorldId = 3, ServerId = 12},
+        {Id = "1区", Name = "自己服务器", Status = "新服", Ip = "10.10.0.206", Port = 5555, WorldId = 3, ServerId = 4},
+        {Id = "2区", Name = "我的外网", Status = "火爆", Ip = "106.52.135.65", Port = 5555, WorldId = 3, ServerId = 4},
+        {Id = "3区", Name = "外网windows", Status = "火爆", Ip = "121.5.104.175", Port = 5555, WorldId = 3, ServerId = 4},
+
     }
     --]]
 --end
